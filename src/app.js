3f4526c59e8ac245712e423cd3120006cb97d18a
agpasha9@gmail.com <agpasha9@gmail.com>
2021-10-19 00:02:06 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,11 @@
+console.log("Merhaba Kodlama.io")
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0; i<konutKredileri.length; i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
